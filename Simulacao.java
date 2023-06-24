8a484fac3d6b26541f1ec65b357d9e3bbcfc24c4
Ana Clara Gomes <109770449+claragomesac@users.noreply.github.com>
2023-06-24 17:53:20 -0300

    Update Simulacao.java

--- Simulacao.java
+++ Simulacao.java
@@ -2,9 +2,20 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
+
 /**
- * Responsavel pela simulacao.
- * @author David J. Barnes, Michael Kolling, Luiz Merschmann and João Barbosa and Ana Clara
+ * Responsável pela simulação.
+ * Descrição detalhada da classe e suas funcionalidades.
+ * 
+ * Este código é baseado em um trabalho anterior de David J. Barnes, Michael Kolling,
+ * Luiz Merschmann, João Barbosa e Ana Clara.
+ * 
+ * @author David J. Barnes
+ * @author Michael Kolling
+ * @author Luiz Merschmann
+ * @author João Barbosa
+ * @author Ana Clara
+ * @version 2.0
  */
 public class Simulacao {
     private Cliente cliente;
@@ -13,6 +24,10 @@ public class Simulacao {
     private Queue<Cliente> filaCliente;
     private ArrayList<Atendente> listaAtendentes;
     
+    /**
+     * Construtor da classe Simulacao.
+     * Inicializa a simulação e configura os elementos necessários.
+     */
     public Simulacao() {
         Random rand = new Random(12345);
         mapa = new Mapa();
@@ -22,16 +37,23 @@ public class Simulacao {
         filaCliente = new LinkedList<>();
         listaAtendentes = new ArrayList<>();
         criarAtendentes();
-        cliente = new Cliente("Jozé Maria", 6666666, randCli.nextInt(8), new Localizacao(rand.nextInt(largura),rand.nextInt(altura)));//Cria um cliente em uma posicao aleatoria
-        cliente.setLocalizacaoDestino(new Localizacao(rand.nextInt(largura),rand.nextInt(altura)));//Define a posicao destino aleatoriamente
-        mapa.adicionarItem(cliente);//Inicializando o mapa com o veículo
+        cliente = new Cliente("Jozé Maria", 6666666, randCli.nextInt(8), new Localizacao(rand.nextInt(largura),rand.nextInt(altura)));
+        cliente.setLocalizacaoDestino(new Localizacao(rand.nextInt(largura),rand.nextInt(altura)));
+        mapa.adicionarItem(cliente);
         janelaSimulacao = new JanelaSimulacao(mapa);
     }
+    
     private void criarAtendentes(){
         listaAtendentes.add(new Atendente("Ana", 0));
         listaAtendentes.add(new Atendente("Joao", 0));
         listaAtendentes.add(new Atendente("Pedro", 0));
     }
+
+    /**
+     * Executa a simulação por um número específico de passos.
+     * 
+     * @param numPassos O número de passos da simulação a serem executados.
+     */
     public void executarSimulacao(int numPassos){
         janelaSimulacao.executarAcao();
         for (int i = 0; i < numPassos; i++) {
@@ -42,7 +64,7 @@ public class Simulacao {
 
     private void executarUmPasso() {
         mapa.removerItem(cliente);
-        cliente.mover();
+        cliente.executarAcao();
         mapa.adicionarItem(cliente);
         janelaSimulacao.executarAcao();
     }
@@ -53,6 +75,5 @@ public class Simulacao {
         }catch(InterruptedException e){
             System.out.println(e.getMessage());
         }
-    }
-    
+    }   
 }
