2940fdbf66781ba7f405d09847901e723143f5e3
Ana Clara Gomes <109770449+claragomesac@users.noreply.github.com>
2023-07-09 13:26:18 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -4,6 +4,6 @@
 public class Principal {
     public static void main(String[] args) {
         Simulacao sim = Simulacao.getInstanceSimulacao(60,5);
-        sim.executarSimulacao(40);
+        sim.executarSimulacao();
     }
 }
