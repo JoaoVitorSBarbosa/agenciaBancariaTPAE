95dcbe2caffcb8c9de030de6fd7f6d87ddce2e59
JoaoVitorSBarbosa <jvsb2017@gmail.com>
2023-07-11 01:46:41 -0300

    Resolvido bug por Merge

--- Mapa.java
+++ Mapa.java
@@ -43,17 +43,12 @@ public class Mapa {
                     if (i instanceof Cliente) {
                         Cliente aux = (Cliente) i;
                         aux.mover();
-                        if (!aux.foiAtendido() || aux.getLocalizacaoAtual().getY()!=1) {
-                            adicionarItem(aux);
-                        }
-                    } else {
-                        adicionarItem(i);
                     }
+                    adicionarItem(i);
                 }
             }
         }
     }
-    
     
     public Item getItem(int x, int y){
         return itens[x][y];
